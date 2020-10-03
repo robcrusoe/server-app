106635b6a343da59a7f735215309a0adadb923f0
robcrusoe <arka.sain@aol.com>
2020-10-03 16:53:51 +0530

    Understanding ViewEncapsulation (Emulated | None)

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.Emulated // Default
+  encapsulation: ViewEncapsulation.None // Removes ViewEncapsulation | Styles put in for this Component will be applicable to All Components *
 })
 export class ServerElementComponent implements OnInit {
 
